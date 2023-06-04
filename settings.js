d4f6e774113229f13a7ad46b6a7b2fde4a400b22
sidlainungan <135518903+sidlainungan@users.noreply.github.com>
2023-06-04 22:20:46 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6285397904282','6285397904282','6285397904282'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6285397904282'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6285397904282' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'PEMDES-LAINUNGAN' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'PEMDES-LAINUNGAN' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'LAINUNGAN' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
@@ -41,8 +41,8 @@ global.email = 'riychdwaynee@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = 'PEMDES-LAINUNGAN' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'LAINUNGAN' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
